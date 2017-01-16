a95eccc6371c4953bcda2d4bb9a16f7f937b5cfa
Israel Manzo <israman814@gmail.com>
2017-01-16 15:00:09 -0500

    Update with a refresh view controller

--- My News/My News/ViewController.swift
+++ My News/My News/ViewController.swift
@@ -8,30 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchResultsUpdating {
     
     var newsArticle = [NewsArticle]()
     
     var closure = NewsModel()
+    
+    var refreshControl: UIRefreshControl!
+    var timer: Timer!
+    var isAnimating = false
+    
+//    var filterNews:[NewsArticle] = []
+    var searchController: UISearchController!
+    var resultsController = UITableViewController()
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        searchBar.delegate = self
-        
         closure.fetchData(with: {listArticles in
             self.newsArticle = listArticles!
             self.tableView.reloadData()
         })
+        // searchController
+        searchController = UISearchController(searchResultsController: self.resultsController)
+        self.tableView.tableHeaderView = self.searchController.searchBar
+        self.searchController.searchResultsUpdater = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        searchBar.delegate = self
+        refreshControl = UIRefreshControl()
+        tableView.addSubview(refreshControl)
+        
+        
+        
     }
     
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    func updateSearchResults(for searchController: UISearchController) {
+//        self.filterNews.filter { (news:[NewsArticle]) -> Bool in
+//            if (news as AnyObject).contains(self.searchController.searchBar.text!) { return true}
+//            else {return false}
+//        }
     }
+    
     
     // MARK: Delegates functions
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,6 +79,24 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             let myIndexPath = self.tableView.indexPathForSelectedRow!
             let row = myIndexPath.row
             destinationVC.webSite = newsArticle[row].url
+        }
+    }
+    
+    func doSomething() {
+        timer = Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(ViewController.endWork), userInfo: nil, repeats: true)
+    }
+    
+    func endWork(){
+        refreshControl.endRefreshing()
+        timer.invalidate()
+        timer = nil
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        if refreshControl.isRefreshing {
+            if !isAnimating {
+                doSomething()
+            }
         }
     }
 }
