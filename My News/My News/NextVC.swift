14e305b6f731225ace0a007e42abd4300fe94f8e
Israel Manzo <israman814@gmail.com>
2017-07-30 21:30:40 -0400

    Updating segue on table view menu slide

---
+++ My News/My News/NextVC.swift
@@ -0,0 +1,23 @@
+//
+//  NextVC.swift
+//  My News
+//
+//  Created by Israel Manzo on 7/30/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class NextVC: UIViewController {
+
+    @IBOutlet weak var menu: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menu.target = revealViewController()
+        menu.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+    }
+
+}
--- My News/My News/NextViewController.swift
+++ My News/My News/NextViewController.swift
@@ -4,17 +4,37 @@
 //
 //  Created by Israel Manzo on 7/30/17.
 //  Copyright © 2017 Israel Manzo. All rights reserved.
-//
+
 
 import UIKit
 
-class NextViewController: UIViewController {
+class NextViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cell = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cell)
+        
+        cell?.textLabel?.text = "Menu Show"
+        
+        return cell!
+    }
 
 }
