298c21227d130a22b999839aba854744a99e1a88
Israel Manzo <israman814@gmail.com>
2017-08-05 09:58:58 -0400

    Adding collection view to a new view controller

--- My News/My News/EntertainmentViewController.swift
+++ My News/My News/EntertainmentViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class EntertainmentViewController: UIViewController {
+class EntertainmentViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     @IBOutlet weak var menu: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
         menu.target = revealViewController()
         menu.action = #selector(SWRevealViewController.revealToggle(_:))
@@ -23,6 +28,20 @@ class EntertainmentViewController: UIViewController {
         
         view.addGestureRecognizer(revealViewController().panGestureRecognizer())
 
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
 
 }
