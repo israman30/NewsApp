b7421df3f4ece54b296a3f958dcec73d586a13cb
Israel Manzo <israman814@gmail.com>
2017-08-08 15:36:07 -0400

    Updating a swift file model

---
+++ My News/My News/TechModel.swift
@@ -0,0 +1,16 @@
+//
+//  TechModel.swift
+//  My News
+//
+//  Created by Israel Manzo on 8/8/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import Foundation
+
+
+class TechModel {
+
+
+
+}
