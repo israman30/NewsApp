2d74e6e87014721a9e034a14b37c07de321832e5
Israel Manzo <israman814@gmail.com>
2017-08-08 14:05:22 -0400

    Updating table view file

--- My News/My News/TechViewController.swift
+++ My News/My News/TechViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class TechViewController: UIViewController {
+class TechViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBOutlet weak var menu: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         menu.target = revealViewController()
         menu.action = #selector(SWRevealViewController.revealToggle(_:))
@@ -22,6 +27,19 @@ class TechViewController: UIViewController {
         navigationController?.navigationBar.barTintColor = UIColor.black
         
         view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cell")
+        return cell!
     }
 
    
