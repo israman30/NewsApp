e3823900bba9284f493f988be2f3216dc0e0185c
Israel Manzo <israman814@gmail.com>
2017-08-08 14:02:56 -0400

    Adding table view cell file

---
+++ My News/My News/TechTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TechTableViewCell.swift
+//  My News
+//
+//  Created by Israel Manzo on 8/8/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class TechTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
