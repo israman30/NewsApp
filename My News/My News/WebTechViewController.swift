38feeb3a20c7bea6a35e2bdbc2e31afb8cc08a2a
Israel Manzo <israman814@gmail.com>
2017-08-08 14:25:14 -0400

    Added sgue

---
+++ My News/My News/WebTechViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebTechViewController.swift
+//  My News
+//
+//  Created by Israel Manzo on 8/8/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class WebTechViewController: UIViewController {
+    
+    var webSite: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
