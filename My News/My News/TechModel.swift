0fba165f6dc0022f7b3e99cf9391fd743d351ab1
Israel Manzo <israman814@gmail.com>
2017-08-08 15:49:43 -0400

    Update fetch data with closure

--- My News/My News/TechModel.swift
+++ My News/My News/TechModel.swift
@@ -11,6 +11,41 @@ import Foundation
 
 class TechModel {
 
+    func parseJson(data: Data, completion: @escaping([TechArticle]?)->()){
+    
+        var techArticles:[TechArticle] = []
+        
+        if let jasonObject = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [String:Any]{
+            
+            let readArticles = jasonObject["articles"] as! [[String:Any]]
+            
+            for newAtricles in readArticles {
+                let articles = TechArticle(data: newAtricles)
+                techArticles.append(articles)
+            }
+            
+            DispatchQueue.main.async {
+                completion(techArticles)
+            }
+        }
+    }
+    
+    func fetchData(with closure: @escaping([TechArticle]?)->()){
+        
+        let url = ""
+        let request = URL(string: url)!
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let dataResponse = data else {
+                closure(nil)
+                return
+            }
+            self.parseJson(data: dataResponse, completion: closure)
+        }
+        task.resume()
+    
+    }
 
 
+    
 }
