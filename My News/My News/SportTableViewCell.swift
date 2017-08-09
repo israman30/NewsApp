2f00af274937b938595e46aad8cfc75853f7740f
Israel Manzo <israman814@gmail.com>
2017-08-09 17:56:27 -0400

    Adding model and network files

---
+++ My News/My News/SportTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SportTableViewCell.swift
+//  My News
+//
+//  Created by Israel Manzo on 8/9/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class SportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ My News/My News/SportsArticle.swift
@@ -0,0 +1,13 @@
+//
+//  SportsArticle.swift
+//  My News
+//
+//  Created by Israel Manzo on 8/9/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import Foundation
+
+class SportsArticle {
+
+}
