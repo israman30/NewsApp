87f06aa5eebcd4f89405bcb9ff1069a7dee04f47
Israel Manzo <israman814@gmail.com>
2017-08-09 13:10:03 -0400

    Updating labels fetching data

--- My News/My News/MoreNewsViewController.swift
+++ My News/My News/MoreNewsViewController.swift
@@ -14,12 +14,21 @@ class MoreNewsViewController: UIViewController, UITableViewDelegate, UITableView
 
     @IBOutlet weak var menu: UIBarButtonItem!
     
+    var moreNews = [MoreNewsArticles]()
+    
+    var closure = MoreNewsModel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        closure.fetchData { (article) in
+            self.moreNews = article!
+            self.tableView.reloadData()
+        }
         
         menu.target = revealViewController()
         menu.action = #selector(SWRevealViewController.revealToggle(_:))
@@ -42,11 +51,14 @@ class MoreNewsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return moreNews.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! MoreNewsTableViewCell
+        
+        cell.titleLbl.text = moreNews[indexPath.row].title
+        cell.dateLbl.text = moreNews[indexPath.row].publishedAt
         
         return cell
     }
