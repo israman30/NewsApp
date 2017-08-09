1891a796b70549ee0bac761195e0f8153b2ac6a0
Israel Manzo <israman814@gmail.com>
2017-08-09 17:53:05 -0400

    Adding table view on sports view controller

--- My News/My News/SecondViewController.swift
+++ My News/My News/SecondViewController.swift
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBOutlet weak var menu: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         menu.target = revealViewController()
         menu.action = #selector(SWRevealViewController.revealToggle(_:))
         
@@ -23,5 +28,19 @@ class SecondViewController: UIViewController {
         
         view.addGestureRecognizer(revealViewController().panGestureRecognizer())
         
-    }   
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "Hello World"
+        return cell!
+    }
 }
---
+++ My News/My News/WebSportViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WebSportViewController.swift
+//  My News
+//
+//  Created by Israel Manzo on 8/9/17.
+//  Copyright © 2017 Israel Manzo. All rights reserved.
+//
+
+import UIKit
+
+class WebSportViewController: UIViewController {
+    
+    var webSite: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    
+
+}
