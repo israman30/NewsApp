17d1560e2924eb39bb25930f28444be45942c82b
Israel Manzo <israman814@gmail.com>
2017-12-24 16:37:03 -0500

    Updating MVC

--- My News/My News/NewsTableViewCell.swift
+++ My News/My News/NewsTableViewCell.swift
@@ -35,13 +35,4 @@ class NewsTableViewCell: UITableViewCell {
             }
         }
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-    }
 }
