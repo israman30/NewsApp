bd894111e78b11a486832c94e43441cbd7e985d5
Israel Manzo <israelmanzo814@gmail.com>
2020-06-08 17:36:51 -0400

    Updating unit test for index path

--- MyNewNews/MyNewNewsTests/MyNewNewsTests.swift
+++ MyNewNews/MyNewNewsTests/MyNewNewsTests.swift
@@ -36,7 +36,7 @@ class MyNewNewsTests: XCTestCase {
         XCTAssertEqual(articles.publishedAt, articlesViewModel.publishedAt)
         XCTAssertEqual(articles.urlToImage, articlesViewModel.urlToImage)
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
