4384b8964671efc158c283eae8c62d0aab7fd24a
Israel Manzo <israelmanzo@israels-mbp-2.myfiosgateway.com>
2022-12-26 22:42:13 -0500

    Added constants file for endpoint

---
+++ MyNewNews/MyNewNews/Utils/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  MyNewNews
+//
+//  Created by Israel Manzo on 12/26/22.
+//  Copyright © 2022 Israel Manzo. All rights reserved.
+//
+
+import Foundation
+
+
+class Constants {
+    static let headingEndpoint = "https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey="
+    static let bodyEndpoint = "https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey="
+}
